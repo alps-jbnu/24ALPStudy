3a19b2959081d90bdd50730328c9ed4a8120806a
les151 <163518700+les151@users.noreply.github.com>
2024-03-21 15:49:53 +0900

    Create 15552.cpp

---
+++ 2024-1/Basic/les151/Basic2/15552.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	int T;
+    cin >> T;
+	while (T--) {
+		int a, b;
+		cin >> a >> b;
+		cout << a + b << "\n";
+	}
+	return 0;
+}
