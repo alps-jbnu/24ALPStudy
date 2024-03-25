e38f24ef1ef048c58502624406053d6e82b9dfc6
igaeun8 <145009182+igaeun8@users.noreply.github.com>
2024-03-26 00:06:10 +0900

    Create 1427.cpp

---
+++ 2024-1/Basic/igaeun8/sort/1427.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+#include <algorithm>
+
+
+using namespace std;
+
+/* sort 알고리즘 내림차순 정렬
+sort(시작 지점, 끝 지점, greater<char>())*/
+		   
+int main(int argc, char **argv)
+{
+	
+	string str;
+
+
+	cin >> str;
+	
+	
+	sort(str.begin(), str.end(), greater<char>());
+
+	cout << str;
+
+	return 0;
+}
