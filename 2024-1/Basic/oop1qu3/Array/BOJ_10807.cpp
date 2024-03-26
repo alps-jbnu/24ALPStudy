f5ad643c74e182eb1f70cd992017e8b48ca012d8
oop1qu3 <144473528+oop1qu3@users.noreply.github.com>
2024-03-27 00:42:28 +0900

    Create BOJ_10807.cpp

---
+++ 2024-1/Basic/oop1qu3/Array/BOJ_10807.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+#define fastio cin.tie(0)->sync_with_stdio(0)
+using namespace std;
+
+int main() 
+{
+    fastio;
+
+    int N; cin >> N;
+    int freq[201] = {};
+
+    int k = 0;
+    for (int i = 0; i < N; i++)
+    {
+        cin >> k;
+        if (k < 0) freq[201 + k]++;
+        else freq[k]++;
+    }
+
+    int v, ans; cin >> v;
+    if (v < 0) ans = freq[201 + v];
+    else ans = freq[v];
+    
+    cout << ans;
+}
