0bf8bb6deb53308e4cbdf58bd9e728097cccb9c3
igaeun8 <145009182+igaeun8@users.noreply.github.com>
2024-03-31 19:18:35 +0900

    Update 10828.cpp

--- 2024-1/Basic/igaeun8/stack/10828.cpp
+++ 2024-1/Basic/igaeun8/stack/10828.cpp
@@ -1,71 +1,36 @@
 #include <iostream>
-#include <string>
 #include <stack>
+
 using namespace std;
 
-int main()
-{
-    stack<int> s;
-    int N;
-    string str;
-    int num;
-    int result = 0;
+stack<int> s;
+string str;
+int N,X;
 
+int main() {
     cin >> N;
 
-    for(int i = 0; i < N; i++){
+    for(int i=0;i<N;i++) {
         cin >> str;
 
-        //1. push
-        if(str == "push"){
-            cin >> num;
-            s.push(num);
-        }
-
-        //2.pop
-        else if(str == "pop"){
-            if(s.size() == 0){
-                result = -1;
-                cout << result << endl;
-            }
-            else{
-                result = s.top();
-                cout << result << endl;
+        if(str == "push") {
+            cin >> X;
+            s.push(X);
+        } 
+        else if(str == "top") {
+            if(s.empty()) cout << "-1\n";
+            else cout << s.top() << "\n";
+        } 
+        else if(str == "pop") {
+            if(s.empty()) cout << "-1\n";
+            else {
+                cout << s.top() << "\n";
                 s.pop();
             }
-        }
-
-        //3. size
-        else if(str == "size"){
-               cout << s.size() << endl;
-        }
-
-        //4. empty
-        else if (str == "empty")
-        {
-            if(s.size() == 0){
-                result = 1;
-                   cout << result << endl;
-            }
-            else{
-                result = 0;
-                cout << result << endl;
-            }
-        }
-        
-        //5. top
-        else if (str == "top")
-        {
-            if(s.size() == 0){
-                result = -1;
-                cout << result << endl;
-            }
-            else{
-                result = s.top();
-                cout << result << endl;
-            }  
-        }
-     }
-        return 0;
+        } 
+        else if (str == "empty") 
+            cout << s.empty() << "\n";
+        else if (str == size")
+            cout << s.size() << "\n";
     }
-
+}
