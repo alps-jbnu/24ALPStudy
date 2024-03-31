8621259027651231e4e0c1b816f8843d59fdc435
igaeun8 <145009182+igaeun8@users.noreply.github.com>
2024-03-31 17:18:12 +0900

    Update 10845.cpp

--- 2024-1/Basic/igaeun8/queue/10845.cpp
+++ 2024-1/Basic/igaeun8/queue/10845.cpp
@@ -1,82 +1,55 @@
-
 #include <iostream>
-#include <string>
 #include <queue>
 using namespace std;
-
-int main()
+ 
+int main(void)
 {
-    queue<int> q;
-    int N;
-    string str;
-    int num;
-    int result = 0;
+ 
+	int N, X;
+	queue<int> Q;
+	string str;
+	cin >> N;
+ 
+	for (int i = 0; i < N; i++) {
+		cin >> str;
 
-    cin >> N;
-
-    for(int i = 0; i < N; i++){
-        cin >> str;
-
-        //1. push
-        if(str == "push"){
-            cin >> num;
-            q.push(num);
-        }
+        //1.push
+		if (str == "push") {
+			cin >> X;
+			Q.push(X);
+		}
 
         //2.pop
-        else if(str == "pop"){
-            if(q.size() == 0){
-                result = -1;
-                cout << result << endl;
-            }
-            else{
-                result = q.front();
-                cout << result << endl;
-                q.pop();
-            }
-        }
+		else if (str == "pop") {
+			if (Q.empty()) cout << -1 << '\n';
+			else {
+				cout << Q.front() << '\n';
+				Q.pop();
+			}
+		}
 
-        //3. size
-        else if(str == "size"){
-               cout << q.size() << endl;
-        }
+        //3.size
+		else if (str == "size") {
+			cout << Q.size() << '\n';
+		}
 
-        //4. empty
-        else if (str == "empty")
-        {
-            if(q.size() == 0){
-                result = 1;
-                   cout << result << endl;
-            }
-            else{
-                result = 0;
-                cout << result << endl;
-            }
-        }
+        //4.empty
+		else if (str == "empty") {
+			cout << Q.empty() << '\n';
+		}
         
-        //5. front
-        else if (str == "front")
-        {
-            if(q.size() == 0){
-                result = -1;
-                cout << result << endl;
-            }
-            else{
-                result = q.front();
-                cout << result << endl;
-            }  
-        }
-         else if (str == "back")
-        {
-            if(q.size() == 0){
-                result = -1;
-                cout << result << endl;
-            }
-            else{
-                result = q.back();
-                cout << result << endl;
-            }  
-        }
-     }
-        return 0;
-    }
+        //5.front
+		else if (str == "front") {
+			if (Q.empty()) cout << -1 << '\n';
+			else cout << Q.front() << '\n';
+		}
+
+        //6.back
+		else if (str == "back") {
+			if (Q.empty()) cout << -1 << '\n';
+			else cout << Q.back() << '\n';
+		}
+	}
+ 
+	return 0;
+}
