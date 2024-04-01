2f79334546939b40e2b7488dfa4d27901920c94f
oop1qu3 <144473528+oop1qu3@users.noreply.github.com>
2024-04-01 23:40:39 +0900

    Create BOJ_1874.cpp

---
+++ 2024-1/Basic/oop1qu3/Stack/BOJ_1874.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+#define fastio std::cin.tie(0)->sync_with_stdio(0)
+using namespace std;
+
+int n = 0;
+int input_buffer[100001] = {};
+char output_buffer[200000] = {};
+int* iiter = nullptr;
+char* oiter = nullptr;
+stack<int> stk;
+
+void push()
+{
+	int num = *(iiter++);
+	stk.push(num);
+	*(oiter++) = '+';
+}
+
+void pop()
+{
+	stk.pop();
+	*(oiter++) = '-';
+}
+
+bool makeSequence()
+{
+	for (int i = 0; i < n + 1; i++)
+		input_buffer[i] = i + 1;
+
+	for (int i = 0; i < n; i++)
+	{
+		int k; cin >> k;
+		while (*iiter <= k) push();
+		if (stk.top() != k) return false;
+		pop();
+	}
+	
+	return true;
+}
+
+int main()
+{
+	fastio;
+
+	iiter = input_buffer;
+	oiter = output_buffer;
+	cin >> n;
+
+	if (makeSequence())
+		for (int i = 0; i < 2 * n; i++) cout << output_buffer[i] << '\n';
+	else cout << "NO";
+}
