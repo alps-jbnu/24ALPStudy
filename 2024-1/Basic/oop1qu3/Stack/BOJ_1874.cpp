ec5942c51adf409c6f2031d872c56bd59da5f834
oop1qu3 <144473528+oop1qu3@users.noreply.github.com>
2024-04-02 19:14:07 +0900

    Update BOJ_1874.cpp

--- 2024-1/Basic/oop1qu3/Stack/BOJ_1874.cpp
+++ 2024-1/Basic/oop1qu3/Stack/BOJ_1874.cpp
@@ -2,55 +2,36 @@
 #define fastio std::cin.tie(0)->sync_with_stdio(0)
 using namespace std;
 
+
 int n = 0;
-int input_buffer[100001] = {};
-char output_buffer[200000] = {};
-int* iiter = nullptr;
-char* oiter = nullptr;
+vector<char> output;
 stack<int> stk;
 
 bool makeSequence();
-void push();
-void pop();
 
 int main()
 {
 	fastio;
 
-	iiter = input_buffer;
-	oiter = output_buffer;
 	cin >> n;
 
 	if (makeSequence())
-		for (int i = 0; i < 2 * n; i++) cout << output_buffer[i] << '\n';
+		for (char c : output) cout << c << '\n';
 	else cout << "NO";
 }
 
 bool makeSequence()
 {
-	for (int i = 0; i < n + 1; i++)
-		input_buffer[i] = i + 1;
+	int input = 1;
 
 	for (int i = 0; i < n; i++)
 	{
 		int k; cin >> k;
-		while (*iiter <= k) push();
+		while (input <= k) { stk.push(input++); output.push_back('+'); }
+
 		if (stk.top() != k) return false;
-		pop();
+		stk.pop(); output.push_back('-');
 	}
 
 	return true;
-}
-
-void push()
-{
-	int num = *(iiter++);
-	stk.push(num);
-	*(oiter++) = '+';
-}
-
-void pop()
-{
-	stk.pop();
-	*(oiter++) = '-';
 }
