f16ac3d0d990cd50db38fe571b07cf5c85baf5b7
CloudSnow <131899719+CloudSnowRaccoon@users.noreply.github.com>
2024-05-14 16:22:43 +0900

    Update 11729.cpp

--- 2024-1/Basic/cloudsnow123/Recursive/11729.cpp
+++ 2024-1/Basic/cloudsnow123/Recursive/11729.cpp
@@ -2,22 +2,22 @@
 
 using namespace std;
 
-void func(int a, int b, int n) {
+void hanoi(int n, int src, int dst, int via) {
     if (n == 1) {
-        cout << a << " " << b << "\n";
+        cout << src << " " << dst << "\n";
         return;
     }
-    func(a, 6 - a - b, n - 1);
-    cout << a << " " << b << "\n";
-    func(6 - a - b, b, n - 1);
+    hanoi(n - 1, src, via, dst);
+    cout << src << " " << dst << "\n";
+    hanoi(n - 1, via, dst, src);
 }
 
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
-    int k;
-    cin >> k;
-    cout << (1 << k) - 1 << "\n";       // 1<<k 비트 연산자 ex) 3 = 0011, 3<<1 = 0110 = 6 3<<2 = 1100 = 12 ...
-    func(1, 3, k);
+    int n;
+    cin >> n;
+    cout << (1 << n) - 1 << "\n";       // 1<<k 비트 연산자 ex) 3 = 0011, 3<<1 = 0110 = 6 3<<2 = 1100 = 12 ...
+    hanoi(n, 1, 3, 2);
     return 0;
 }
