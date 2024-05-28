c246f23e0f40a048ee7f39bf185ec4d59b34a3e3
JUNE-E <36994762+JUNE-E@users.noreply.github.com>
2024-05-29 01:15:57 +0900

    Update 7662.cpp

--- 2024-1/Basic/dark6409/BST/7662.cpp
+++ 2024-1/Basic/dark6409/BST/7662.cpp
@@ -1,3 +1,4 @@
+
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -8,24 +9,26 @@ int main() {
   int t;
   cin >> t;
 
-  multiset<int> s; // 오름차순으로 정렬됨
   while(t--){
+    multiset<int> s; // 오름차순으로 정렬됨
     int k;
     cin >> k;
     while(k--){
-      char order; int n;
-      cin >> order >> n;
+      char order;
+      cin >> order;
 
-      if(order == 'I') s.insert(n);
-      else {
+      if(order == 'D') {
+        int n; cin >> n;
         if(s.empty()) continue;
         if(n==1) s.erase(prev(s.end())); // "D 1"은 최댓값을 삭제하는 연산이랬잖아. s.end() 는 마지막 원소의 한 칸 뒤를 가리키기 때문에, prev() 으로 접근
         else s.erase(s.begin());
       }
+      else {
+        int n; cin >> n;
+        s.insert(n);}
     }
-    if(s.empty()) cout << "EMPTY" << '\n';
-    else{cout << *prev(s.end()) << ' ' << *s.begin() << '\n';}
+    if(s.empty()) cout << "EMPTY\n";
+    else {
+      cout << *prev(s.end()) << ' ' << *s.begin() << '\n';}
   }
-
-  return 0;
 }
