070905dbcfeb819de994d8ad2b8b43c66549b09f
kdw7849 <dongone5717@gmail.com>
2024-07-17 00:52:36 +0900

    Create 10808.cpp

---
+++ 여름방학/Basic/kdw7849/array/10808.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<string.h>
+
+int main() {
+	int alphabet[26] = { NULL };
+	char word[101] = { NULL };
+	scanf("%s", &word, 101);
+	for (int i = 0; i < strlen(word); i++) {
+		alphabet[word[i] - 'a']++;
+	}
+	for (int i = 0; i < 26; i++) {
+		printf("%d ", alphabet[i]);
+	}
+	return 0;
+}
